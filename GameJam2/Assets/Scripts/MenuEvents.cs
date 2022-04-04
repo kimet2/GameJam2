c78f4b7e66929f747830e7b19cc4c2c450978e48
Abdelmajid Ghaiati <a2mjidgh@gmail.com>
2022-04-04 16:05:18 +0200

    Main Menu
    I Finished the Main menu

---
+++ GameJam2/Assets/Scripts/MenuEvents.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuEvents : MonoBehaviour
+{
+    public void LoadGame(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
