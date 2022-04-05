37c4d420358ffc0be21697f74a0ec3d6098da907
kimet2 <joaquimorteu1@gmail.com>
2022-04-05 09:49:17 +0200

    A
    a

--- GameJam2/Assets/Scripts/First Game Script/Player.cs
+++ GameJam2/Assets/Scripts/First Game Script/Player.cs
@@ -1,26 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Player : MonoBehaviour
 {
-    public float playerSpeed;
-    private Rigidbody2D rb;
-    private Vector2 playerDirection;
+    //public static Action<string> PlayerCollectsCoin = delegate { };
 
+    private Rigidbody2D rb;
+    private float dirX, dirY, moveSpeed;
+
+    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        moveSpeed = 5f;
     }
 
+    // Update is called once per frame
     void Update()
     {
-        float directionY = Input.GetAxisRaw("Vertical");
-        playerDirection = new Vector2(0, directionY).normalized;
+        if (name == "PlayerOne")
+        {
+            //dirX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+            dirY = Input.GetAxisRaw("Vertical") * moveSpeed;
+        }
+
+        if (name == "PlayerTwo" && Input.anyKey)
+        {
+            if (Input.GetKey(KeyCode.I))
+            {
+                dirY = moveSpeed;
+            }
+
+            if (Input.GetKey(KeyCode.K))
+            {
+                dirY = -moveSpeed;
+            }
+
+            /* if (Input.GetKey(KeyCode.J))
+            {
+                dirX = - moveSpeed;
+            }
+
+            if (Input.GetKey(KeyCode.L))
+            {
+                dirX = moveSpeed;
+            } */
+        }        
+        else if (name == "PlayerTwo" && !Input.anyKey)
+        {
+            //dirX = 0f;
+            dirY = 0f;
+        }
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        rb.velocity = new Vector2(0, playerDirection.y * playerSpeed);
+        rb.velocity = new Vector2(dirX, dirY);
     }
+
+    /* private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Coin>())
+        {
+            PlayerCollectsCoin(name);
+            Destroy(collision.gameObject);
+        }
+    } */
 }
