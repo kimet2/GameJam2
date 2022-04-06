a994cd49e792c85af7cb68c80147256ff45015b8
Abdelmajid Ghaiati <a2mjidgh@gmail.com>
2022-04-06 13:12:50 +0200

    game 3
    changes

--- GameJam2/Assets/Scripts/Third Game Script/CameraMovement.cs
+++ GameJam2/Assets/Scripts/Third Game Script/CameraMovement.cs
@@ -8,6 +8,7 @@ public class CameraMovement : MonoBehaviour
 
     void Update()
     {
+        cameraSpeed += 0.1f * Time.deltaTime;
         transform.position += new Vector3(cameraSpeed * Time.deltaTime, 0, 0);
     }
 }
